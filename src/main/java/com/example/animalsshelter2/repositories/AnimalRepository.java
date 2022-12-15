f4d5a787a19dca6741fa388f39a6f9ad92cad287
kristiyandimov19 <unconfigured@null.spigotmc.org>
2022-12-15 12:55:18 +0200

    animal-shelter-2

--- src/main/java/com/example/animalsshelter2/repositories/AnimalRepository.java
+++ src/main/java/com/example/animalsshelter2/repositories/AnimalRepository.java
@@ -4,8 +4,8 @@ import com.example.animalsshelter2.models.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
+
 }
